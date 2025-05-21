69f7c1cb07488277cd936fd640f2f8658d3d9c18
rikhilramaswamy <nikhmr@gmail.com>
2025-05-21 12:49:30 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
 OPENAI_API_KEY =""
-LLM_MODEL_NAME = "gpt-3.5-turbo-0125"
-SITEMAP_URL=“https://zerodha.com/varsity/chapter-sitemap2.xml”
+LLM_MODEL_NAME = "gpt-3.5-turbo-0125
+SITEMAP_URL="https://zerodha.com/varsity/chapter-sitemap2.xml"
