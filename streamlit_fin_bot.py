ed4783110d9e03c2f1481cf4a84d78b2adfcf995
rikhilramaswamy <nikhmr@gmail.com>
2025-05-22 10:43:58 +0530

    Update streamlit_fin_bot.py
    1) use session state to store chat history
    2) contextualise the prompt using chat history

--- streamlit_fin_bot.py
+++ streamlit_fin_bot.py
@@ -6,6 +6,8 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
+from langchain.chains import create_history_aware_retriever
+from langchain_core.prompts import MessagesPlaceholder
 from langchain_community.document_loaders import WebBaseLoader
 import bs4
 from langchain_community.document_loaders import RecursiveUrlLoader
@@ -72,33 +74,49 @@ def vector_retriever(docs):
 										embedding=oi_embeddings)
 	return vectorstore.as_retriever()
 
-def create_chain(url):
+def create_rag_chain(url):
 	docs = scrape_site(url)
 	retriever = vector_retriever(docs)
 	# 2. Incorporate the retriever into a question-answering chain.
-	system_prompt = (
-	    "You are a financial assistant for question-answering tasks. "
-	    "Use the following pieces of retrieved context to answer "
-	    "the question. If you don't know the answer, say that you "
-	    "don't know. Use three sentences maximum and keep the "
-	    "answer concise."
-	    "If the question is not clear ask follow up questions"
-	    "\n\n"
-	    "{context}"
-	)
 
-	prompt = ChatPromptTemplate.from_messages(
-	    [
-	        ("system", system_prompt),
-	        ("human", "{input}"),
-	    ]
-	)
+	contextualize_q_system_prompt = (
+    "Given a chat history and the latest user question "
+    "which might reference context in the chat history, "
+    "formulate a standalone question which can be understood "
+    "without the chat history. Do NOT answer the question, "
+    "just reformulate it if needed and otherwise return it as is."
+    )
 
 	llm = ChatOpenAI(model=LLM_MODEL_NAME)
 
-	question_answer_chain = create_stuff_documents_chain(llm, prompt)
-	return create_retrieval_chain(retriever, question_answer_chain)
+	history_aware_retriever = create_history_aware_retriever(
+		llm, retriever, contextualize_q_system_prompt
+	)
 
+	# Example of how to create a QA prompt (ensure system_prompt is defined)
+	system_prompt = (
+		"You are a financial assistant for question-answering tasks. "
+		"Use the following pieces of retrieved context to answer "
+		"the question. If you don't know the answer, say that you "
+		"don't know. Use three sentences maximum and keep the "
+		"answer concise."
+		"If the question is not clear ask follow up questions"
+		"\n\n"
+		"{context}"
+	)
+
+	qa_prompt = ChatPromptTemplate.from_messages(
+		[
+			("system", system_prompt),
+			MessagesPlaceholder("chat_history"),
+			("human", "{input}"),
+		]
+	)
+
+	question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+	rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+	return rag_chain
+      
 
 st.title("RAG based Financial ChatBot")
 
@@ -114,10 +132,18 @@ user_input = st.text_input("Enter your question below:", "Ask, Bot!")
 # IOW: create_chain() API is invoked only on APP init for the first time
 # on subsequent query rag_chain object created on init is re-used
 if 'rag_chain' not in st.session_state:
-	st.session_state['rag_chain'] = create_chain(SITEMAP_URL)
+	st.session_state['rag_chain'] = create_rag_chain(SITEMAP_URL)
 	st.write("invoking data fetch to create rag chain")
 
-response = st.session_state['rag_chain'].invoke({"input": user_input})
+# use session state to store chat history
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = []
+
+
+response = st.session_state['rag_chain'].invoke({"input": user_input,
+                                                 "chat_history": st.session_state['messages']}) 
+# Append the user input and bot response to the messages list
+st.session_state['messages'].append((user_input, response["answer"]))
 
 # Add a button. The st.button() function returns True if the button was clicked,
 # and False otherwise.
